e8d1977243f7a9b4523f3eeda3265affc0e5a4e1
Samay Piduru <sgpiduru@ncsu.edu>
2023-04-30 22:34:52 -0400

    removed vapid functionality for now

--- api/src/modules/push.js
+++ api/src/modules/push.js
@@ -1,14 +1,14 @@
-const webpush = require('web-push');
+// const webpush = require('web-push');
 
-const publicKey = process.env.VAPID_PUBLIC;
-const privateKey = process.env.VAPID_PRIVATE;
+// const publicKey = process.env.VAPID_PUBLIC;
+// const privateKey = process.env.VAPID_PRIVATE;
 
 function setup() {
-    webpush.setVapidDetails(
-        "mailto:a@b.c",
-        publicKey,
-        privateKey
-    );
+    // webpush.setVapidDetails(
+    //     "mailto:a@b.c",
+    //     publicKey,
+    //     privateKey
+    // );
 }
 
 module.exports = {
