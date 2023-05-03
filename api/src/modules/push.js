c62a61c4e827c27f8a3ba2ecc8c96dc9666ec2e8
Samay Piduru <sgpiduru@ncsu.edu>
2023-05-03 13:16:06 -0400

    added module for push notifs

--- api/src/modules/push.js
+++ api/src/modules/push.js
@@ -1,16 +1,32 @@
-// const webpush = require('web-push');
+const webpush = require('web-push');
 
-// const publicKey = process.env.VAPID_PUBLIC;
-// const privateKey = process.env.VAPID_PRIVATE;
+const email = process.env.EMAIL_ADDRESS;
+const publicKey = process.env.VAPID_PUBLIC;
+const privateKey = process.env.VAPID_PRIVATE;
 
-function setup() {
-    // webpush.setVapidDetails(
-    //     "mailto:a@b.c",
-    //     publicKey,
-    //     privateKey
-    // );
+// configure VAPID
+webpush.setVapidDetails(
+  `mailto: ${email}`,
+  publicKey,
+  privateKey
+);
+
+// Function to send a push notification
+async function notifyNow(subscription, payload) {
+  try {
+    const response = await webpush.sendNotification(subscription, JSON.stringify(payload));
+    console.log('Push notification sent successfully:', response);
+  } catch (error) {
+    console.error('Error sending push notification:', error);
+  }
+}
+
+async function notifyAt(subscription, payload, date) {
+  if(date < Date.now()) return;
+  const delay = date - Date.now();
+  setTimeout(() => notifyNow(subscription, payload), delay);
 }
 
 module.exports = {
-    setup
-}
+  notifyNow, notifyAt
+};
