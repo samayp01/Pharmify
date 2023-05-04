afcad94e1e9bfe996d2adda1119cf04c161f428b
Samay Piduru <sgpiduru@ncsu.edu>
2023-05-04 00:35:54 -0400

    set env vars globally in build process

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,9 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true,
 
   devServer: {
     allowedHosts: "all",
     port: 80
+  },
+  chainWebpack: config => {
+    config.plugin('define').tap(args => {
+      args[0]['process.env'].VUE_APP_VAPID_PUBLIC_KEY = JSON.stringify(process.env.VUE_APP_VAPID_PUBLIC_KEY);
+      return args;
+    });
   }
 })
